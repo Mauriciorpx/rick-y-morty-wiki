4cc1d88fd814329c5b178c77241f071ec3c285e1
Mauricio <mauricio3741gmail.com>
2022-06-23 22:19:24 -0500

    Pagination

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 /* import logo from './logo.svg'; */
-import './App.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios'
-import SearchBox from './components/SearchBox';
-import ResidentsList from './components/ResidentsList';
-import LocationInfo from './components/LocationInfo';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import SearchBox from "./components/SearchBox";
+import ResidentsList from "./components/ResidentsList";
+import LocationInfo from "./components/LocationInfo";
 
 function App() {
-
   const [location, setLocation] = useState({});
+  const [suggetions, setSuggetions] = useState([]);
 
-  useEffect(() =>{
+  useEffect(() => {
     /* Random number for a random location => Return a random number between 0 and 125, so we need add 1 */
-    const random = Math.floor(Math.random() * 126) +1;
+    const random = Math.floor(Math.random() * 126) + 1;
     /* For dynamic code, we use backpics on the url */
-    axios.get(`https://rickandmortyapi.com/api/location/${random}`)
-    .then(res => setLocation(res.data))
+    axios
+      .get(`https://rickandmortyapi.com/api/location/${random}`)
+      .then((res) => setLocation(res.data));
   }, []);
-  
-  console.log(location)
+
+  console.log(location);
 
   return (
     <div className="App">
-      <SearchBox setLocation = {setLocation}/>
-      
-      <LocationInfo location = {location}/>
-      
-      <ResidentsList residents ={location?.residents}/>
+      <SearchBox setLocation={setLocation} />
+
+      <LocationInfo location={location} />
+
+      <ResidentsList residents={location?.residents} />
     </div>
   );
 }
--- src/components/ResidentsList.js
+++ src/components/ResidentsList.js
@@ -1,18 +1,60 @@
-import React from 'react';
-import ResidentInfo from './ResidentInfo';
+import React from "react";
+import { useState } from "react";
+import ResidentInfo from "./ResidentInfo";
 
-const ResidentsList = ({residents}) => {
-    return (
-        <div className='resident-list'>
-            {
-                residents?.map(resident =>(
+const ResidentsList = ({ residents }) => {
+  /* se crea un nuevo arreglo */
 
-                    <ResidentInfo residentUrl = {resident} key={resident}/>
-                
-                ))
-            }
-        </div>
-    );
+  const [page, setPage] = useState(1);
+  const characterNumber = 6;
+
+  const lastIndex = characterNumber * page;
+  const firstIndex = lastIndex - characterNumber;
+  /* Se realiza un slice */
+  const characterPaginated = residents?.slice(firstIndex, lastIndex);
+
+  /* Determinar ultima pagina */
+  const lastPage = Math.ceil(residents?.length / characterNumber);
+
+  /* Numeros de paginas */
+
+  const numbers = [];
+  for (let i = 1; i <= lastPage; i++) {
+    numbers.push(i);
+  }
+
+  return (
+    <div className="resident-list">
+      <div>
+        {" "}
+        <ul>
+          {characterPaginated?.map((resident) => (
+            <ResidentInfo residentUrl={resident} key={resident} />
+          ))}
+        </ul>
+        <button
+          className="nav-btn"
+          onClick={() => setPage(page - 1)}
+          disabled={page === 1}
+        >
+          Prev
+        </button>
+        {/* Map for number of pages */}
+        {numbers.map((number) => (
+          <button className="btn-numbers" onClick={() => setPage(number)}>
+            {number}
+          </button>
+        ))}
+        <button
+          className="nav-btn"
+          onClick={() => setPage(page + 1)}
+          disabled={page === lastPage}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
 
 /* <div key={resident}>
